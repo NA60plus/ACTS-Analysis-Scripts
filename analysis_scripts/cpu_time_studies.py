0f9a7f5ceb77ddbe0ec587fe6e7dbfc91f0575cf
galocco <giacomo.alocco@cern.ch>
2024-10-17 16:14:17 +0200

    update cpu time studies

--- analysis_scripts/cpu_time_studies.py
+++ analysis_scripts/cpu_time_studies.py
@@ -61,7 +61,7 @@ def get_run_time(dir):
 
 
 
-def draw_runtime_pie(dict_time, title, suffix =""):
+def draw_runtime_pie(dict_time, title, directory = "../results/cpu_time_studies", suffix =""):
     """
     Function to draw a pie chart with the running time for each algorithm
 
@@ -88,6 +88,7 @@ def draw_runtime_pie(dict_time, title, suffix =""):
         i += 1
 
     cv = ROOT.TCanvas("cv","cv",1600,1600)
+    #cv.SetRightMargin(0.)
 
     pie.Draw()
 
@@ -97,9 +98,9 @@ def draw_runtime_pie(dict_time, title, suffix =""):
     pieleg.SetX1(.7)
     pieleg.SetX2(1)
  
-    cv.SaveAs("results/cpu_time_studies/pie"+suffix+".png")
+    cv.SaveAs(directory+"/pie"+suffix+".png")
 
-def draw_step_comparison_pie(list_time, algo = "Algorithm:TrackFindingAlgorithm", suffix =""):
+def draw_step_comparison_pie(list_time, algo = "Algorithm:TrackFindingAlgorithm", directory = "../results/cpu_time_studies", suffix =""):
     """
     Function to draw a pie chart with the running time of an algorithm for each step
 
@@ -140,7 +141,7 @@ def draw_step_comparison_pie(list_time, algo = "Algorithm:TrackFindingAlgorithm"
     pieleg.SetX1(.8)
     pieleg.SetX2(.95)
  
-    cv.SaveAs("results/cpu_time_studies/pieStep"+suffix+".png")
+    cv.SaveAs(directory+"/pieStep"+suffix+".png")
 
 def print_expected_cpu_time(fit_function, multiplicity_ratio, centrality_max = 50, multiplicity_scale_factor = 1, n_collisions = 10**11):
     """
@@ -166,50 +167,36 @@ def print_expected_cpu_time(fit_function, multiplicity_ratio, centrality_max = 5
 
 def main():
 
-    # Directory name
-    directory = "results/cpu_time_studies"
-    # Create the directory
-    os.makedirs(directory, exist_ok=True)
-
+    #directories containing timing.tsv
     dir_list = [
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.01",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.05",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.1",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.2",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.4",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.6",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_periferal_factor_0.8",
-        "results/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1"
+                "/home/giacomo/acts_for_NA60+/ACTS-Analysis-Scripts/output/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_realTarget_beam0.5_twosteps_rej0.114_perigeeZ400_suffix",
+                "/home/giacomo/acts_for_NA60+/ACTS-Analysis-Scripts/output/output_40GeV_newSeeding_standardSeeding_deadZones_maxSeedSpMPrim1_maxSeedSpMSec20_ImpMax1_dZMax50_branch1_realTarget_beam0.5_fluka_twosteps_rej0.114_perigeeZ400_suffix"
         ]
+    #list of names for the plots
     suffix_list = [
-                "periferal_factor_0.01",
-                "periferal_factor_0.05",
-                "periferal_factor_0.1",
-                "periferal_factor_0.2",
-                "periferal_factor_0.4",
-                "periferal_factor_0.6",
-                "periferal_factor_0.8",
-                "central"
+                "No fluka",
+                "Fluka"
                 ]
+    #fraction of multiplicity in central collisions
     mult_list = [
                     0.01,
-                    0.05,
-                    0.1,
-                    0.2,
-                    0.4,
-                    0.6,
-                    0.8,
-                    1
+                    0.05
                 ]
 
+
+    # Directory name
+    directory = "../results/cpu_time_studies"
+    # Create the directory
+    os.makedirs(directory, exist_ok=True)
 
     time_graph = ROOT.TGraphErrors(len(dir_list))
     time_graph.SetTitle("; <Multiplicity>/<Multiplicity 40 GeV, 0-5%>; CPU time / event (s)")
 
     for i, (dir, suffix, mult) in enumerate(zip(dir_list, suffix_list, mult_list)):
         tot_time, dict_time, list_time = get_run_time(dir=dir)
-        draw_runtime_pie(dict_time, "Time/event = " + str(round(tot_time, 3)) + " s", suffix=suffix)
-        draw_step_comparison_pie(list_time, suffix=suffix)
+        print(dict_time)
+        draw_runtime_pie(dict_time, "Time/event = " + str(round(tot_time, 3)) + " s", directory, suffix)
+        draw_step_comparison_pie(list_time, directory=directory, suffix=suffix)
         time_graph.SetPoint(i, mult, tot_time)
         time_graph.SetPointError(i, 0, 0)  # Assuming no error, if there's an error value, replace 0 with the error
 
@@ -221,22 +208,27 @@ def main():
     time_graph.Draw("AP")
     t = ROOT.TText(.5,.5,"CPU time = C x N_{trk}^"+str(round(fit_function.GetParameter(0),2)))
     t.Draw()
-    cv.SaveAs("results/cpu_time_studies/timevsMultiplicity.png")
+    cv.SaveAs(directory+"/timevsMultiplicity.png")
     cv.SetLogy()
-    cv.SaveAs("results/cpu_time_studies/timevsMultiplicityLog.png")
+    cv.SaveAs(directory+"/timevsMultiplicityLog.png")
 
     # multiplicity_energy = pions + kaons + pions + decay dauthers from K0s->pipi and lambda->ppi
     multiplicity158 = 1258.000000 + 155.000000 +  292.000000 + 77.45*2*0.692 + 47.97*2*0.639
     multiplicity40 = 615.000000 + 78.000000 + 150.000000 + 39.15*2*0.692 + 43*2*0.639
+    multiplicity20 = 410 + 51 + 148. + (39.15*2*0.692 + 43*2*0.639)/2.
     
     # list of <npart>/<npart 0-5%> for each 5% centrality class from 0 to 80%(50%) from a Glauber simulation with E = 158 AGeV
     ratio80 = [1.0, 0.7301204819277108, 0.6144578313253012, 0.5180722891566265, 0.43614457831325304, 0.363855421686747, 0.29759036144578316, 0.2421686746987952, 0.19518072289156627, 0.15421686746987953, 0.12048192771084337, 0.09156626506024096, 0.06746987951807229, 0.04819277108433735, 0.033734939759036145, 0.02289156626506024]
     ratio50 = [1.0, 0.7301204819277108, 0.6144578313253012, 0.5180722891566265, 0.43614457831325304, 0.363855421686747, 0.29759036144578316, 0.2421686746987952, 0.19518072289156627, 0.15421686746987953]
 
     print("==========================================================")
-    print("E =40 AGeV, centrality = 0-50%")
+    print("E = 20 AGeV, centrality = 0-50%")
+    print_expected_cpu_time(fit_function, ratio50, 50, multiplicity20/multiplicity40)
+    print("E = 20 AGeV, centrality = 0-80%")
+    print_expected_cpu_time(fit_function, ratio80, 80, multiplicity20/multiplicity40)
+    print("E = 40 AGeV, centrality = 0-50%")
     print_expected_cpu_time(fit_function, ratio50, 50)
-    print("E =40 AGeV, centrality = 0-80%")
+    print("E = 40 AGeV, centrality = 0-80%")
     print_expected_cpu_time(fit_function, ratio80, 80)
     print("E = 158 AGeV, centrality = 0-50%")
     print_expected_cpu_time(fit_function, ratio50, 50, multiplicity158/multiplicity40)
